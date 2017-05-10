1e2882f9da213d46daea7314b2d13a1ea422e585
Rafael Costa <rafaelmbcosta@gmail.com>
2017-05-09 23:45:21 -0300

    Listing recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Bolo", "This is a Cake",  "https://www.bbcgoodfood.com/sites/default/files/styles/category_retina/public/chocolate-avocado-cake.jpg?itok=E2eWE_Dx"),
+    new Recipe("Tapioca", "This is a Tapioca",  "http://cevisa.org.br/blog/wp-content/uploads/2015/10/tapioca.jpg")
+  ];
 
   constructor() { }
 
